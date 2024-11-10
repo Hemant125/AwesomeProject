d6bb525fd728b834ccf1a914e9bed6951eb2b160
Hemant <hemantkumaryadav1131@gmail.com>
2024-11-10 22:38:41 +0530

    "new"

---
+++ Timer.js
@@ -0,0 +1,16 @@
+import react from "react"
+
+import {Text, View} from "react-native"
+
+const Timer =()=>{
+ return(
+
+    <View>
+        <Text>
+            hemant
+        </Text>
+    </View>
+ )
+}
+
+export default Timer
--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './Timer';
 import {name as appName} from './app.json';
+import Timer from './Timer';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Timer);
